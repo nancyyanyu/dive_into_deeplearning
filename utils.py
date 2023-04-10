e54dedb843dd5a16c59ce47f59e72477ee3da641
Nancy <nancy@NancydeMBP.home>
2023-04-10 10:57:54 -0700

    change utils

--- utils.py
+++ utils.py
@@ -385,6 +385,7 @@ class Classifier(Module):
             print(layer.__class__.__name__,  'output shape:\t', X.shape)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 class LinearRegressionKeras(Module):
     def __init__(self, lr):
@@ -449,6 +450,31 @@ class TimeMachine(DataModule):
         return self.get_tensorloader([self.X, self.Y], train, idx)
 
 >>>>>>> 2624afc... ch9
+=======
+
+class LinearRegressionKeras(Module):
+    def __init__(self, lr):
+        super().__init__()
+        self.save_hyperparameters()
+        initializer = tf.initializers.RandomNormal(stddev=0.01)
+        # only generate a single scalar output, so set the parameter to 1
+        self.net = tf.keras.layers.Dense(1, kernel_initializer=initializer)
+        
+    def forward(self, X):
+        # invoke the built-in __call__ method of the predefined layers to compute the outputs.
+        return self.net(X)
+    
+    def loss(self, y_hat, y):
+        fn = tf.keras.losses.MeanSquaredError()
+        return fn(y_hat, y)
+    
+    def configure_optimizers(self):
+        return tf.keras.optimizers.SGD(self.lr)
+    
+    def get_w_b(self):
+        return self.get_weights()[0], self.get_weights()[1]
+    
+>>>>>>> ae5a7ac... change utils
     
 def cpu():
     return tf.device('/CPU:0')
