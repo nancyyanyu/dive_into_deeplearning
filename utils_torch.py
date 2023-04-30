c6d0eb10997deee8decf7232dd368a3101531536
nancyyanyu <yy2799@columbia.edu>
2023-04-30 15:35:07 -0700

    update ch10 ch11

--- utils_torch.py
+++ utils_torch.py
@@ -783,9 +783,13 @@ class RNNLMScratch(Classifier):
         return ''.join([vocab.idx_to_token[i] for i in outputs])
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     
 =======
 >>>>>>> 74c8dcd... ch10
+=======
+    
+>>>>>>> 4299bd5... update ch10 ch11
 class RNN(Module):
     """The RNN model implemented with high-level APIs.
     Defined in :numref:`sec_rnn-concise`"""
@@ -881,4 +885,87 @@ class MTFraEng(DataModule):
 
     def forward(self, inputs, H=None):
         return self.rnn(inputs, H)
+<<<<<<< HEAD
 >>>>>>> 74c8dcd... ch10
+=======
+
+class GRU(RNN):
+    """The multi-layer GRU model.
+    Defined in :numref:`sec_deep_rnn`"""
+    def __init__(self, num_inputs, num_hiddens, num_layers, dropout=0):
+        Module.__init__(self)
+        self.save_hyperparameters()
+        self.rnn = nn.GRU(num_inputs, num_hiddens, num_layers,
+                          dropout=dropout)
+        
+        
+class MTFraEng(DataModule):
+    """The English-French dataset."""
+    def __init__(self, batch_size, num_steps=9, num_train=512, num_val=128):
+        """Defined in :numref:`sec_machine_translation`"""
+        super(MTFraEng, self).__init__()
+        self.save_hyperparameters()
+        self.arrays, self.src_vocab, self.tgt_vocab = self._build_arrays(
+            self._download())
+        
+    def _download(self):
+        extract(download(
+            DATA_URL+'fra-eng.zip', self.root))
+        with open(self.root + '/fra-eng/fra.txt', encoding='utf-8') as f:
+            return f.read()
+
+    def _preprocess(self, text):
+        """Defined in :numref:`sec_machine_translation`"""
+        # Replace non-breaking space with space
+        text = text.replace('\u202f', ' ').replace('\xa0', ' ')
+        # Insert space between words and punctuation marks
+        no_space = lambda char, prev_char: char in ',.!?' and prev_char != ' '
+        out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char
+               for i, char in enumerate(text.lower())]
+        return ''.join(out)
+
+    def _tokenize(self, text, max_examples=None):
+        """Defined in :numref:`sec_machine_translation`"""
+        src, tgt = [], []
+        for i, line in enumerate(text.split('\n')):
+            if max_examples and i > max_examples: break
+            parts = line.split('\t')
+            if len(parts) == 2:
+                # Skip empty tokens
+                src.append([t for t in f'{parts[0]} <eos>'.split(' ') if t])
+                tgt.append([t for t in f'{parts[1]} <eos>'.split(' ') if t])
+        return src, tgt
+
+    def _build_arrays(self, raw_text, src_vocab=None, tgt_vocab=None):
+        def _build_array(sentences, vocab, is_tgt=False):
+            pad_or_trim = lambda seq, t: (
+                seq[:t] if len(seq) > t else seq + ['<pad>'] * (t - len(seq)))
+            sentences = [pad_or_trim(s, self.num_steps) for s in sentences]
+            if is_tgt:
+                sentences = [['<bos>'] + s for s in sentences]
+            if vocab is None:
+                vocab = Vocab(sentences, min_freq=2)
+            array = tensor([vocab[s] for s in sentences])
+            valid_len = reduce_sum(
+                astype(array != vocab['<pad>'], int32), 1)
+            return array, vocab, valid_len
+        src, tgt = self._tokenize(self._preprocess(raw_text),
+                                  self.num_train + self.num_val)
+        src_array, src_vocab, src_valid_len = _build_array(src, src_vocab)
+        tgt_array, tgt_vocab, _ = _build_array(tgt, tgt_vocab, True)
+        return ((src_array, tgt_array[:,:-1], src_valid_len, tgt_array[:,1:]),
+                src_vocab, tgt_vocab)
+
+    def get_dataloader(self, train):
+        """Defined in :numref:`subsec_loading-seq-fixed-len`"""
+        idx = slice(0, self.num_train) if train else slice(self.num_train, None)
+        return self.get_tensorloader(self.arrays, train, idx)
+
+    def build(self, src_sentences, tgt_sentences):
+        """Defined in :numref:`subsec_loading-seq-fixed-len`"""
+        raw_text = '\n'.join([src + '\t' + tgt for src, tgt in zip(
+            src_sentences, tgt_sentences)])
+        arrays, _, _ = self._build_arrays(
+            raw_text, self.src_vocab, self.tgt_vocab)
+        return arrays
+>>>>>>> 4299bd5... update ch10 ch11
