f45a3f4e1206cd3c7276713021d85bfe144a758c
nancyyanyu <yy2799@columbia.edu>
2023-05-06 22:01:26 -0700

    ch13

--- utils_torch.py
+++ utils_torch.py
@@ -1219,3 +1219,25 @@ class TransformerEncoder(Encoder):
             self.attention_weights[
                 i] = blk.attention.attention.attention_weights
         return X
+    
+    
+class Timer:
+    def __init__(self):
+        self.times=[]
+        self.start()
+        
+    def start(self):
+        self.tik = time.time()
+        
+    def stop(self):
+        self.times.append(time.time()-self.tik)
+        return self.times[-1]
+    
+    def avg(self):
+        return sum(self.times)/len(self.times)
+    
+    def sum(self):
+        return sum(self.times)
+    
+    def cumsum(self):
+        return np.array(self.times).cumsum().tolist()
